ef999075ee5b4924b842f97e782383ed55849c4a
Stanislav Semeniuk <ssemeniu@redhat.com>
2026-05-07 11:59:00 +0200

    fix: aligned tsup.config.ts externals with peer dependencies from package.json

--- packages/core/tsup.config.ts
+++ packages/core/tsup.config.ts
@@ -6,5 +6,5 @@ export default defineConfig({
   dts: true,
   sourcemap: true,
   clean: true,
-  external: ["react"],
+  external: ["react", "react-dom", "@patternfly/react-core", "@patternfly/react-icons"],
 });
