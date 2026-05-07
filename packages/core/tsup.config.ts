b5cd4af3f0d36e5b6cee1a41e7ab47118a8bd31e
Stanislav Semeniuk <ssemeniu@redhat.com>
2026-05-07 12:07:27 +0200

    fix: aligned tsup config with peer dependencies in every sublib, made it work dynamically

--- packages/core/tsup.config.ts
+++ packages/core/tsup.config.ts
@@ -1,10 +1,12 @@
 import { defineConfig } from "tsup";
 
+import pkg from "./package.json";
+
 export default defineConfig({
   entry: ["src/index.ts"],
   format: ["esm", "cjs"],
   dts: true,
   sourcemap: true,
   clean: true,
-  external: ["react", "react-dom", "@patternfly/react-core", "@patternfly/react-icons"],
+  external: [...Object.keys(pkg.peerDependencies || {})],
 });
--- packages/table-controls/tsup.config.ts
+++ packages/table-controls/tsup.config.ts
@@ -1,10 +1,14 @@
 import { defineConfig } from "tsup";
 
+import pkg from "./package.json";
+
 export default defineConfig({
   entry: ["src/index.ts"],
   format: ["esm", "cjs"],
   dts: true,
   sourcemap: true,
   clean: true,
-  external: ["react", "@tsd-ui/core"],
+  external: [
+    ...Object.keys(pkg.peerDependencies || {}),
+  ],
 });
